1bf3fb1184e204af0f893c92bedf8897d1b4c396
dhjw <dhjw@protonmail.com>
2025-04-09 09:40:14 -0700

    Improve things

--- koyfin_cs.js
+++ koyfin_cs.js
@@ -29,20 +29,42 @@ function init() {
 
 function update() {
 	if (!document.hasFocus()) return
-	// don't use cache in case assets or rows are edited or re-init on navigation
+	// don't use cache in case assets, rows or columns are edited or moved or re-init on navigation
 	mwt = document.querySelector('div[class^=my-watchlist-table]')
 	if (!mwt) return
-	// console.log('[koyfin-enhance] update')
-	prices = mwt.querySelectorAll('div[class*=cell-content-animated-price-update]')
-	assets = mwt.querySelectorAll('div[class^=table-cell-user-data__tableCellUserData__label]')
-	for (i = 0; i < assets.length; i++) {
+	// get and process column headings
+	let headers = mwt.querySelectorAll('div[class^=table-styles__table__headerCell]')
+	let indexes = {}
+	for (let i = 0; i < headers.length; i++) {
+		if (headers[i].innerText === 'Exchange') {
+			indexes.exchange = i;
+			continue
+		}
+		if (headers[i].innerText === 'Last Price') {
+			indexes.price = i;
+			continue
+		}
+		if (headers[i].innerText === heading_str) {
+			indexes.asset = i;
+			continue
+		}
+		if (indexes.length >= 3) break;
+	}
+	// get and process rows
+	let rows = mwt.querySelectorAll('div[class^=table-styles__table__row]')
+	for (let i = 0; i < rows.length - 1; i++) { // ignore last row
+		let cells = rows[i].querySelectorAll('div[class^=table-styles__table__dataCell]')
+		let exchangeText = indexes.exchange ? $(cells[indexes.exchange]).text() : null // no consistent label element, jquery is easier
+		let priceText = cells[indexes.price].querySelector('div[class^=default-cell__label]').innerText
+		let assetLabel = cells[indexes.asset].querySelector('div[class^=table-cell-user-data__tableCellUserData__label]')
+
 		// console.log('i:', i, 'text: "' + assets[i].innerText + '"')
-		let m = assets[i].innerText.match(new RegExp('^(\\d+) @ (\\d+.?(?:\\d+)?)(?: ([^( ]*))?'))
+		let m = assetLabel.innerText.match(new RegExp('^(\\d+) @ (\\d+.?(?:\\d+)?)(?: ([^( ]*))?'))
 		if (m) {
 			// console.log('m:', m)
 			// 740 @ 3.38 CAD (+1.04 +12.1% +$1550)
-			// TODO: currency conversion support
-			let cur_price = parseFloat(prices[i].innerText)
+			// TODO: currency conversion and asset total value
+			let cur_price = parseFloat(priceText)
 			let buy_amt = parseFloat(m[1])
 			let buy_price = parseFloat(m[2]).toFixed(2)
 			let currency = m[3] ? m[3] : ''
@@ -50,7 +72,7 @@ function update() {
 			let diff_perc = ((cur_price - buy_price) / buy_price * 100).toFixed(1)
 			let diff_amt = ((buy_amt * cur_price) - (buy_amt * buy_price)).toFixed(2)
 			// console.log([cur_price, buy_amt, buy_price, currency, diff_price, diff_perc, diff_amt])
-			assets[i].innerText = buy_amt + ' @ ' + buy_price + (currency ? ' ' + currency : '') + ' (' + (diff_price >= 0 ? '+' : '') + diff_price + ' ' + (diff_perc >= 0 ? '+' : '') + diff_perc + '% ' + (diff_amt >= 0 ? '+' : '') + diff_amt + ')'
+			assetLabel.innerText = buy_amt + ' @ ' + buy_price + (currency ? ' ' + currency : '') + ' (' + (diff_price >= 0 ? '+' : '') + diff_price + ' ' + (diff_perc >= 0 ? '+' : '') + diff_perc + '% ' + (diff_amt >= 0 ? '+' : '') + diff_amt + ')'
 		}
 	}
 }
